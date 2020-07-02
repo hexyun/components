fe64ebab2499e09cbac7a3130eef5620f82278a8
wangyanjing <wangyanjing@wudaima.com>
2020-07-02 14:42:36 +0800

    首页动画

--- examples/main.js
+++ examples/main.js
@@ -15,40 +15,50 @@ Vue.config.debug = true;
 var router = new VueRouter();
 
 router.map({
-  '/waterfall': {
-    component: function(resolve) {
-      require(['./routers/waterfall.vue'], resolve);
+    '/waterfall': {
+        component: function(resolve) {
+            require(['./routers/waterfall.vue'], resolve);
+        }
+    },
+    '/navcate': {
+        component: function(resolve) {
+            require(['./routers/navcate.vue'], resolve);
+        }
+    },
+    '/area': {
+        component: function(resolve) {
+            require(['./routers/area.vue'], resolve);
+        }
+    },
+    '/timeline': {
+        component: function(resolve) {
+            require(['./routers/timeline.vue'], resolve);
+        }
+    },
+    '/datepicker': {
+        component: function(resolve) {
+            require(['./routers/datepicker.vue'], resolve);
+        }
+    },
+    '/hxkdate': {
+        component: function(resolve) {
+            require(['./routers/hxk_date.vue'], resolve);
+        }
+    },
+    '/mybutton': {
+        component: function(resolve) {
+            require(['./routers/mybutton.vue'], resolve);
+        }
+    },
+    '/imgscroll': {
+        component: function(resolve) {
+            require(['./routers/imgscroll.vue'], resolve);
+        }
     }
-  },
-  '/navcate': {
-    component: function(resolve) {
-      require(['./routers/navcate.vue'], resolve);
-    }
-  },
-  '/area': {
-    component: function(resolve) {
-      require(['./routers/area.vue'], resolve);
-    }
-  },
-  '/timeline': {
-    component: function(resolve) {
-      require(['./routers/timeline.vue'], resolve);
-    }
-  },
-  '/datepicker': {
-    component: function(resolve) {
-      require(['./routers/datepicker.vue'], resolve);
-    }
-  },
-  '/hxkdate': {
-    component: function(resolve) {
-      require(['./routers/hxk_date.vue'], resolve);
-    }
-  }
 });
 
 router.beforeEach(function() {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 });
 
 router.afterEach(function(transition) {
@@ -56,6 +66,6 @@ router.afterEach(function(transition) {
 });
 
 router.redirect({
-  '*': "/hxkdate"
+    '*': "/hxkdate"
 });
 router.start(App, '#app');
--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ import area from "./components/area/index.vue";
 import timeline from "./components/timeline/index.vue";
 import datepicker from "./components/datepicker/index.vue";
 import hxkdate from './components/hxk_date/index.vue'
+import mybutton from './components/mybutton/index.vue'
+import imgscroll from './components/imgscroll/index.vue'
 const Components = {
-  navCate,
-  waterfall,
-  area,
-  timeline,
-  datepicker,
-  hxkdate
+    navCate,
+    waterfall,
+    area,
+    timeline,
+    datepicker,
+    hxkdate,
+    mybutton,
+    imgscroll
 };
 
-const install = function (Vue, opts = {}) {
-  Object.keys(Components).forEach((key) => {
-    Vue.component(Components[key].name || key, Components[key]);
-  });
+const install = function(Vue, opts = {}) {
+    Object.keys(Components).forEach((key) => {
+        Vue.component(Components[key].name || key, Components[key]);
+    });
 };
 
 // auto install
 if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
+    install(window.Vue);
 }
 
 module.exports = Object.assign(Components, { install }); // eslint-disable-line no-undef
