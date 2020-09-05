6b47b8fcd3cde923c5f96338b64442a7a3e67f07
mixcoding <fangxiaoyu@hexdo.com>
2020-09-05 18:01:46 +0800

    feat  级联选择样式调整

--- examples/routers/navcate.vue
+++ examples/routers/navcate.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class='wrap'>
+  <div class="wrap">
     <button @click="cli">cli</button>
-    <nav-cate :default-data="defaultData" :event-type="eventType" :list="list" :level='level' @select="selectNode"></nav-cate>
+    <nav-cate
+      :default-data="defaultData"
+      :event-type="eventType"
+      :list="list"
+      :level="level"
+      @select="selectNode"
+    ></nav-cate>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      eventType:'click',//hover
-      defaultData:[
+      eventType: "click", //hover
+      defaultData: [
         {
           id: "car_11",
           name: "车分类",
@@ -21,17 +27,17 @@ export default {
         {
           id: "car_31",
           name: "车分类",
-        }
+        },
       ],
       level: 3,
       list: [
         {
-          name: "车分类1",
+          name: "儿童摄影展",
           id: "car_11",
           children: [
             {
               id: "car_21",
-              name: "车二级分类字数超多11"
+              name: "车二级分类字数超多11",
             },
             {
               id: "car_22",
@@ -39,42 +45,53 @@ export default {
               children: [
                 {
                   id: "car_31",
-                  name: "三级分类121"
+                  name: "三级分类121",
                 },
                 {
                   id: "car_32",
-                  name: "三级分类122"
+                  name: "三级分类122",
                 },
                 {
                   id: "car_33",
-                  name: "三级分类123"
+                  name: "三级分类123",
                 },
                 {
                   id: "car_34",
-                  name: "三级分类124"
-                }
-              ]
+                  name: "三级分类124",
+                },
+              ],
             },
             {
               id: "car_23",
-              name: "车二级分类字数超多13"
-            },{
-              id: "car_24",
-              name: "车二级分类字数超多14"
+              name: "车二级分类字数超多13",
             },
-          ]
+            {
+              id: "car_24",
+              name: "车二级分类字数超多14",
+            },
+          ],
         },
         {
-          name: "车分类2",
+          name: "婚纱展",
           id: "car_12",
           children: [
             {
               id: "car_222222",
-              name: "车二级分类"
-            }
-          ]
-        }
-      ]
+              name: "车二级分类",
+            },
+          ],
+        },
+        {
+          name: "影响输入设备",
+          id: "car_13",
+          children: [
+            {
+              id: "car_222222",
+              name: "车二级分类",
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
@@ -82,15 +99,15 @@ export default {
     selectNode(thr, sec, fir) {
       console.log(thr, sec, fir);
     },
-    cli () {
+    cli() {
       this.list = [
         {
-          name: "车分类1",
+          name: "儿童摄影展",
           id: "car_11",
           children: [
             {
               id: "car_21",
-              name: "车二级分类字数超多11"
+              name: "测试二级菜单字数限制测试限制限制限制测试限制限制",
             },
             {
               id: "car_22",
@@ -98,46 +115,69 @@ export default {
               children: [
                 {
                   id: "car_31",
-                  name: "三级分类121"
+                  name: "三级分类121",
                 },
                 {
                   id: "car_32",
-                  name: "三级分类122"
+                  name: "三级分类122",
+                },
+                {
+                  id: "car_333",
+                  name: "测试彩色测试",
+                },
+                {
+                  id: "car_344",
+                  name: "三级分类122",
                 },
                 {
                   id: "car_33",
-                  name: "三级分类123"
+                  name: "三级分类123三级分类123三级分类测试测会说出色",
                 },
                 {
                   id: "car_34",
-                  name: "三级分类124"
-                }
-              ]
+                  name: "三级分类124",
+                },
+              ],
             },
             {
               id: "car_23",
-              name: "车二级分类字数超多13"
-            },{
-              id: "car_24",
-              name: "车二级分类字数超多14"
+              name: "车二级分类字数超多13",
             },
-          ]
+            {
+              id: "car_24",
+              name: "车二级分类字数超多14",
+            },
+          ],
         },
         {
-          name: "车分类2",
+          name: "婚纱展",
           id: "car_12",
           children: [
             {
               id: "car_222222",
-              name: "车二级分类"
-            }
-          ]
-        }
-      ]
-    }
+              name: "车二级分类灯光音响设备灯光音响设备灯光音响设备",
+            },
+          ],
+        },
+        {
+          name: "影响输入设备分类",
+          id: "car_13",
+          children: [
+            {
+              id: "car_222333",
+              name: "车二级分类",
+            },
+          ],
+        },
+        {
+          name: "灯光音响设备灯光音响设备灯光音响设备",
+          id: "car_14",
+        },
+      ];
+    },
   },
   ready() {
-    this.cli()
-  }
+    this.cli();
+  },
 };
 </script>
